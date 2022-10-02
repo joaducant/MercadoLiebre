d613a575491f0c942417047a3828efbaf203bea5
joaducant <joaducant@gmail.com>
2022-10-02 11:20:04 -0300

    Terminando de mejorar el CSS

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/views/home2.html"))
+    res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
 app.get("/register", (req,res) => {
